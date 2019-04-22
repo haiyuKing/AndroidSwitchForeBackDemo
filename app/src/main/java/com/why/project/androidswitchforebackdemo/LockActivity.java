0a0d6901a8e5319c8c60f6d32dd144ece6661f3a
haiyuKing <wanghaiyu99999@126.com>
2019-04-22 21:42:33 +0800

    Initial commit

---
+++ app/src/main/java/com/why/project/androidswitchforebackdemo/LockActivity.java
@@ -0,0 +1,18 @@
+package com.why.project.androidswitchforebackdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by haiyuKing
+ * Used 锁屏界面
+ */
+
+public class LockActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lock);
+	}
+}
